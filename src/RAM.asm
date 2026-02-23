2c3a668c1ee5b434ffe41e61cbb93b5eed5d353e
Jonatan HidalgoMorales <jona32u4hm@gmail.com>
2026-02-22 19:45:34 -0600

    completeley re organized memory map for convenience

--- src/RAM.asm
+++ src/RAM.asm
@@ -2,6 +2,14 @@
 
 .dseg
 .org 0x100
+FrameBuffer: .byte 256
+
+.org 0x200
+WRAM: ;this is where the game and variables are stored up to the 2KB limit
+
+
+
+.org 0x7A0
 ;CHIP8 STACK:
 VirtualStack: .byte 16*2 ;stack grows upward in memory (sp inc when push)
 DelayTimer: .byte 1
@@ -9,16 +17,5 @@ SoundTimer: .byte 1
 StackPointer: .byte 1
 FontRAM: .byte 16*5 ;16 characters 5 bytes each
 
-.org 0x200
-WRAM: ;this is where the game and variables are stored up to the 2KB limit
-
-
-
-
-
-.org 0x7FF
-HardwareStack: ;this is the end of the hardware AVR stack which grows downward to lower memory addresses
-.org 0x800
-FrameBuffer: .byte 256
-
+;Hardware stack at the top of WRAM
 ;----------------end of RAM----------------
--- src/chip8.asm
+++ src/chip8.asm
@@ -6,11 +6,10 @@
 ;   0x000 - 0x01F -> 32 Registers
 ;   0x020 - 0x05F -> 64 I/O Registers
 ;   0x060 - 0x0FF -> 160 Ext I/O Regs
-;   0x100 - 0x1FF -> FONTS, Virtual stack, timers, and stack pointer
+;   0x100 - 0x1FF -> 
 ;0x200-0x799 - Program ROM (copy necessary for games that modify their own code) 
 ;                                               and work RAM (up to 2KB limit for ATmega328P)
-;0x7A0-0x7FF hardware stack
-;0x800-0x8FF Framebuffer
+;0x7A0-0x8FF Virtual stack, timers, stack pointer, Fonts, and hardware stack
 ;
 ;--------REGISTERS:--------
 ; V0-15 -> r0-15
