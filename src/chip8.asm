c60d66bcc60448fcdc99982e9fd744f1d7b5b707
Jonatan HidalgoMorales <jona32u4hm@gmail.com>
2026-02-22 19:18:01 -0600

    memory map complete

--- src/chip8.asm
+++ src/chip8.asm
@@ -6,21 +6,14 @@
 ;   0x000 - 0x01F -> 32 Registers
 ;   0x020 - 0x05F -> 64 I/O Registers
 ;   0x060 - 0x0FF -> 160 Ext I/O Regs
-;   0x100 - 0x1FF -> FONTS, STACK, and Other variables
-;0x200-0x8FF - Program ROM (copy necessary for rare games that modify their own code) 
+;   0x100 - 0x1FF -> FONTS, Virtual stack, timers, and stack pointer
+;0x200-0x799 - Program ROM (copy necessary for games that modify their own code) 
 ;                                               and work RAM (up to 2KB limit for ATmega328P)
-
-.dseg
-.org 0x100
-
-
-
-;--------REGISTERS--------
-; V0-15 -> r16-31
-; PC
-; I
+;0x7A0-0x7FF hardware stack
+;0x800-0x8FF Framebuffer
 ;
-
-; Delay timer
-; Sound timer
-
+;--------REGISTERS:--------
+; V0-15 -> r0-15
+; PC    -> r18-19
+; I     -> r26-27 X
+;
